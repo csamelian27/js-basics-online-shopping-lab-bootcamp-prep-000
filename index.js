6c15b641ba4055eee733c9a22216fd031038524a
Cassidy Samelian <csamelian@gmail.com>
2018-12-11 06:45:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,19 +10,44 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*100);
+ cart.push(new Object({itemName: [item], itemPrice: price}));
+ return (`${item} has been added to your cart.`);
 }
 
 function viewCart() {
-  // write your code here
+  var sentence = "In your cart, you have";
+  for (var i = 0; i < cart.length; i++){
+    sentence.push(` ` + cart[i].itemName + " at $" + cart[i].itemPrice + " .");
+  }
+     if (i === 0){
+      return "Your shopping cart is empty.";
 }
+    else if(i === 1){
+      sentence.push(` ` + cart[i].itemName + " at $" + cart[i].itemPrice + ".");
+    }
+    else if(i === 2){
+      sentence.push(", and" + cart[i].itemName + " at $" + cart[i].itemPrice + ".");
+    }
+    else {
+      
+    }
+  }
+
 
 function total() {
   // write your code here
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+ if (Object.keys(cart[i]) == item){
+   cart = [...cart.slice(0, i), ...cart.slice(i+1)];
+   return cart;
+    }
+  }
+ console.log("That item is not in your cart.");
+ return cart;
 }
 
 function placeOrder(cardNumber) {
